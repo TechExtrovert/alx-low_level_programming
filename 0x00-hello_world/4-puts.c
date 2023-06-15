8ed37fb89c76d7f2e07db7cc1b86ae353cc75ba5
TechExtrovert <phreshnezer@gmail.com>
2023-06-15 11:25:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<studio.h>
+/**
+ * main - Entry point
+ * 
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
